3ac3de6354ab9e918b2b011af7da946ec8d0293c
Carloscodec <carlosafdev@gmail.com>
2024-01-10 14:41:44 -0300

    Method 'find id' and increase salary implemented

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -30,12 +30,32 @@ public class Program {
 			list.add(emp);
 		}
 		
+		System.out.print("\nEnter the employee id that will have salary increase: ");
+		int idSalary = input.nextInt();
+		Integer pos = position(list, idSalary);
+		if (pos == null) {
+			System.out.println("This id does not exist!\r\n");
+		} else {
+			System.out.print("\nEnter the percentage: ");
+			double percent = input.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
+		
 		System.out.println("\nList of Employees:");
 		for (Employee x : list) {
 			System.out.println(x);
 		}
 
 		input.close();
+	}
+	
+	public static Integer position(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
 	}
 
 }
--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -34,6 +34,10 @@ public class Employee {
 	public Double getSalary() {
 		return salary;
 	}
+	
+	public void increaseSalary(double percentage) {
+		salary = salary + salary * percentage / 100.0;
+	}
 
 	public String toString() {
 		return "Id: " + getId() + ", " + getName()
